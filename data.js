8e334ca929fae131be212b717f9f66cdfa359c52
WE4EVERKOMBAN <108579654+WE4EVERKOMBAN@users.noreply.github.com>
2022-09-08 18:47:23 +0530

    Update and rename data(1).js to data.js

---
+++ data.js
@@ -0,0 +1,25 @@
+module.exports = {
+
+mongoLink:'mongodb+srv://Suhail:suhail123@cluster0.jjayw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',//database link
+
+bot_token:'5710908115:AAEpaffr33vUpE4NQOj4-RQC5EgMwpeDRTw',
+
+bot_name:'Noyel_Tomo_Bot',//bot username without @
+
+bot_admin:895687774,
+
+channelsList : ['@AirdropAssasins'],
+
+channelscheck : ['@AirdropAssasins'],
+
+airdropName : 'Noyel Tomo.',
+
+reffer_bonus:100000,// ral bonus amount
+
+min_wd:300000,//min withdrawal amount
+
+currency:'rdt', //bot cuurency
+
+payment_channel:'@PayOutToken'// payment channel username
+
+}
